d30148597bc29da43073d2bca1a07c0059610233
光君 <graysheeep@gmail.com>
2016-09-19 18:39:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var qr = require('qr-image')
+var JsBarcode = require('jsbarcode');
+var Canvas = require('canvas');
 var app = express()
 
 app.get('/', function (req, res) {
@@ -15,6 +17,20 @@ app.get('/create_qrcode', function (req, res, next) {
     } catch (e) {
         res.writeHead(414, {'Content-Type': 'text/html'});
         res.end('<h1>414 Request-URI Too Large</h1>');
+    }
+})
+
+app.get('/create_barcode', function (req, res, next){
+    var text = req.query.text;
+    try {
+	var canvas = new Canvas(200, 50);
+	JsBarcode(canvas, text);
+	var img = canvas.pngStream();
+	res.writeHead(200, {'Content-Type': 'image/png'});
+        img.pipe(res);
+    } catch (e) {
+	res.writeHead(414, {'Content-Type': 'text/html'});
+	res.end('<h1>414 Request-URI Too Large</h1>');
     }
 })
 
